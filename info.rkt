b09fa3e58e0532ea2ba596b491c9a98ac731c736
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-06-05 05:18:02 +0000

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define pkg-authors '("ImpishDeathTech@protonmail.ch"))
 (define version "0.1")
 (define pkg-desc "Racket hack of fluffy's isolation engine")
-(define deps '("base" "csfml" "sfml"))
+(define deps '("base" "csfml"))
 
